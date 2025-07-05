60ce9b6534ecde4c497a2469f36f1fba1ca91cc8
Aravind MS <aravind.ms4444@gmail.com>
2025-07-05 11:17:47 +0530

    Commit 10

--- src/tests/purchase-module/test.purchase-flow.ts
+++ src/tests/purchase-module/test.purchase-flow.ts
@@ -12,6 +12,12 @@ describe("Swag Labs iOS App - Purchase Flow", () => {
     await passwordField.setValue("secret_sauce");
     await loginButton.click();
 
+    // Verify Products Text is visible
+    const productsTitle: ChainablePromiseElement = $(
+      '//XCUIElementTypeStaticText[@name="PRODUCTS"]',
+    );
+    await expect(productsTitle).toBeDisplayed();
+
     // Wait for inventory screen
     const firstProduct: ChainablePromiseElement = $(
       '(//XCUIElementTypeOther[@name="test-Item"])[1]',
@@ -24,28 +30,8 @@ describe("Swag Labs iOS App - Purchase Flow", () => {
     );
     await addToCartButton.click();
 
-    // Go to cart
-    const cartButton: ChainablePromiseElement = $("~test-Cart");
-    await cartButton.click();
-
-    // Verify item in cart
-    const cartItem: ChainablePromiseElement = $("~Sauce Labs Backpack");
-    await expect(cartItem).toBeDisplayed();
-
     // Checkout item
     const removeButton: ChainablePromiseElement = $("~test-REMOVE");
     await removeButton.click();
-
-    // Checkout item
-    const continueShoppingButton: ChainablePromiseElement = $(
-      "~test-CONTINUE SHOPPING",
-    );
-    await continueShoppingButton.click();
-
-    // Verify Products Text is visible
-    const productsTitle: ChainablePromiseElement = $(
-      '//XCUIElementTypeStaticText[@name="PRODUCTS"]',
-    );
-    await expect(productsTitle).toBeDisplayed();
   });
 });
